17bf6e8f16b7b98e8ccfcd300458a82ef5778ff2
johannbm <johannes.moskvil@gmail.com>
2025-03-04 12:38:57 +0100

    fix object as promise feil

--- app/src/routes/$id.index.tsx
+++ app/src/routes/$id.index.tsx
@@ -1,21 +1,15 @@
-import { createFileRoute, getRouteApi } from "@tanstack/react-router";
+import { createFileRoute, getRouteApi, Navigate } from "@tanstack/react-router";
 
 const route = getRouteApi("/$id");
 
 export const Route = createFileRoute("/$id/")({
   component: () => {
     const params = route.useParams();
-    const navigate = route.useNavigate();
     const { eksisterendeInntektsmeldinger } = route.useLoaderData();
-
     if (eksisterendeInntektsmeldinger[0] === undefined) {
-      return navigate({
-        from: "/$id",
-        to: "/$id/dine-opplysninger",
-        replace: true,
-      });
+      return <Navigate params={params} replace to={"/$id/dine-opplysninger"} />;
     }
 
-    return navigate({ to: "/$id/vis", params, replace: true });
+    return <Navigate params={params} replace to={"/$id/vis"} />;
   },
 });
