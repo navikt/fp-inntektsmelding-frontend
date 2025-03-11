34cae52152feaf36be7f3dc5438cf849b4545e4e
johannbm <johannes.moskvil@gmail.com>
2025-03-11 10:54:01 +0100

    lag egen Agi context

--- app/src/features/arbeidsgiverinitiert/AgiRot.tsx
+++ app/src/features/arbeidsgiverinitiert/AgiRot.tsx
@@ -1,12 +1,12 @@
-import { InntektsmeldingSkjemaStateProvider } from "~/features/InntektsmeldingSkjemaState";
+import { AgiSkjemaStateProvider } from "~/features/arbeidsgiverinitiert/AgiSkjemaState.tsx";
 import { SkjemaRotLayout } from "~/features/rot-layout/SkjemaRotLayout.tsx";
 
 export const ARBEIDSGIVER_INITERT_ID = "agi";
 
 export const AgiRot = () => {
   return (
-    <InntektsmeldingSkjemaStateProvider skjemaId={ARBEIDSGIVER_INITERT_ID}>
+    <AgiSkjemaStateProvider>
       <SkjemaRotLayout ytelse="SVA" />
-    </InntektsmeldingSkjemaStateProvider>
+    </AgiSkjemaStateProvider>
   );
 };
---
+++ app/src/features/arbeidsgiverinitiert/AgiSkjemaState.tsx
@@ -0,0 +1,115 @@
+import { type ReactNode } from "@tanstack/react-router";
+import type { Dispatch, SetStateAction } from "react";
+import { createContext, useContext } from "react";
+import { z, ZodError } from "zod";
+
+import { ARBEIDSGIVER_INITERT_ID } from "~/features/arbeidsgiverinitiert/AgiRot.tsx";
+import { useSessionStorageState } from "~/features/usePersistedState.tsx";
+import { beløpSchema, logDev } from "~/utils.ts";
+
+/**
+ * Minst streng skjema-state. Denne brukes underveis der mange av feltene er optional fordi de ikke er utfylt enda.
+ */
+export const AgiSkjemaStateSchema = z.object({
+  kontaktperson: z
+    .object({
+      navn: z.string(),
+      telefonnummer: z.string(),
+    })
+    .optional(),
+  skalRefunderes: z
+    .union([
+      z.literal("JA_LIK_REFUSJON"),
+      z.literal("JA_VARIERENDE_REFUSJON"),
+      z.literal("NEI"),
+    ])
+    .optional(),
+  refusjon: z.array(
+    z.object({
+      fom: z.string().optional(),
+      beløp: beløpSchema,
+    }),
+  ),
+});
+
+/**
+ * En strengere skjema state. Her er alle verdiene validert mot skjema-logikken.
+ */
+export const AgiSkjemaStateSchemaValidated = z.object({
+  kontaktperson: z.object({
+    navn: z.string(),
+    telefonnummer: z.string(),
+  }),
+
+  skalRefunderes: z.union([
+    z.literal("JA_LIK_REFUSJON"),
+    z.literal("JA_VARIERENDE_REFUSJON"),
+    z.literal("NEI"),
+  ]),
+  refusjon: z.array(
+    z.object({
+      fom: z.string().optional(),
+      beløp: beløpSchema,
+    }),
+  ),
+});
+
+export type AgiSkjemaState = z.infer<typeof AgiSkjemaStateSchema>;
+export type AgiSkjemaStateValid = z.infer<typeof AgiSkjemaStateSchemaValidated>;
+
+type AgiSkjemaStateContextType = {
+  gyldigAgiSkjemaState?: AgiSkjemaStateValid;
+  agiSkjemaStateError?: ZodError;
+  agiSkjemaState: AgiSkjemaState;
+  setAgiSkjemaState: Dispatch<SetStateAction<AgiSkjemaState>>;
+};
+const AgiSkjemaStateContext = createContext<AgiSkjemaStateContextType | null>(
+  null,
+);
+
+type AgiSkjemaStateProviderProps = {
+  children: ReactNode;
+};
+
+const defaultSkjemaState = {
+  refusjon: [],
+} satisfies AgiSkjemaState;
+
+export const AgiSkjemaStateProvider = ({
+  children,
+}: AgiSkjemaStateProviderProps) => {
+  const [state, setState] = useSessionStorageState<AgiSkjemaState>(
+    ARBEIDSGIVER_INITERT_ID,
+    defaultSkjemaState,
+    AgiSkjemaStateSchema,
+  );
+
+  const gyldigAgiSkjemaState = AgiSkjemaStateSchemaValidated.safeParse(state);
+
+  if (!gyldigAgiSkjemaState.success) {
+    logDev("error", gyldigAgiSkjemaState.error);
+  }
+
+  return (
+    <AgiSkjemaStateContext.Provider
+      value={{
+        agiSkjemaState: state,
+        gyldigAgiSkjemaState: gyldigAgiSkjemaState.data,
+        agiSkjemaStateError: gyldigAgiSkjemaState.error,
+        setAgiSkjemaState: setState,
+      }}
+    >
+      {children}
+    </AgiSkjemaStateContext.Provider>
+  );
+};
+
+/** Henter ut global skjematilstand, og lar deg manipulere den */
+export const useAgiSkjema = () => {
+  const context = useContext(AgiSkjemaStateContext);
+  if (!context) {
+    throw new Error("useAgiSkjema må brukes inne i en AgiSkjemaStateProvider");
+  }
+
+  return context;
+};
