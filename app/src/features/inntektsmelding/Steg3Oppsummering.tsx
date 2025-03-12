02e9fe3d3304c3f8ef05d050c78bc6abc881474c
Johannes Moskvil <johannbm@users.noreply.github.com>
2025-03-12 13:42:30 +0100

    Splitt DTO request typene (#10)

--- app/src/features/inntektsmelding/Steg3Oppsummering.tsx
+++ app/src/features/inntektsmelding/Steg3Oppsummering.tsx
@@ -193,9 +193,7 @@ function lagSendInntektsmeldingRequest(
     arbeidsgiverIdent: opplysninger.arbeidsgiver.organisasjonNummer,
     kontaktperson: skjemaState.kontaktperson,
     startdato: opplysninger.førsteUttaksdato,
-    inntekt: gjeldendeInntekt
-      ? formatStrengTilTall(gjeldendeInntekt)
-      : undefined,
+    inntekt: formatStrengTilTall(gjeldendeInntekt),
     refusjon: refusjon.map((r) => ({
       ...r,
       beløp: formatStrengTilTall(r.beløp),
