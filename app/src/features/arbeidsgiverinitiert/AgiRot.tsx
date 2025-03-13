887223a057151b4e49752516b10f01b24dd9bde2
johannbm <johannes.moskvil@gmail.com>
2025-03-13 12:54:15 +0100

    vis ag navn og nummer

--- app/src/features/arbeidsgiverinitiert/AgiRot.tsx
+++ app/src/features/arbeidsgiverinitiert/AgiRot.tsx
@@ -1,6 +1,7 @@
 import { getRouteApi } from "@tanstack/react-router";
 
 import { AgiSkjemaStateProvider } from "~/features/arbeidsgiverinitiert/AgiSkjemaState.tsx";
+import { useAgiOpplysninger } from "~/features/arbeidsgiverinitiert/useAgiOpplysninger.tsx";
 import { SkjemaRotLayout } from "~/features/rot-layout/SkjemaRotLayout.tsx";
 
 export const ARBEIDSGIVER_INITERT_ID = "agi";
@@ -9,10 +10,23 @@ export const ARBEIDSGIVER_INITERT_SKJEMA_ID = "skjemadata-agi";
 const route = getRouteApi("/agi");
 
 export const AgiRot = () => {
-  const { ytelseType } = route.useSearch();
   return (
     <AgiSkjemaStateProvider>
-      <SkjemaRotLayout ytelse={ytelseType} />
+      <AgiRotComponent />
+    </AgiSkjemaStateProvider>
+  );
+};
+
+const AgiRotComponent = () => {
+  const { ytelseType } = route.useSearch();
+  const opplysninger = useAgiOpplysninger();
+  return (
+    <AgiSkjemaStateProvider>
+      <SkjemaRotLayout
+        organisasjonNavn={opplysninger.arbeidsgiver.organisasjonNavn}
+        organisasjonNummer={opplysninger.arbeidsgiver.organisasjonNummer}
+        ytelse={ytelseType}
+      />
     </AgiSkjemaStateProvider>
   );
 };
