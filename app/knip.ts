22bbe1b1e8f19ba7adc8ca4aeb42203bb013403c
johannbm <johannes.moskvil@gmail.com>
2025-08-05 09:55:08 +0200

    knip

--- app/knip.ts
+++ app/knip.ts
@@ -5,9 +5,10 @@ const config: KnipConfig = {
     "src/routeTree.gen.ts",
     "src/features/react-hook-form-wrappers/DateRangePickerWrapped.tsx",
     "src/types/api-models.ts",
+    "tailwind.config.ts",
   ],
   ignoreBinaries: [],
-  ignoreDependencies: ["@navikt/ds-css"],
+  ignoreDependencies: ["@navikt/ds-css", "@navikt/ds-tailwind"],
 };
 
 export default config;
