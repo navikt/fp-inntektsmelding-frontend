2f539dcbfc8c1ab41009977fea3179821d0250e3
johannbm <johannes.moskvil@gmail.com>
2026-02-17 15:05:27 +0100

    fiks import

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import compression from "vite-plugin-compression2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
