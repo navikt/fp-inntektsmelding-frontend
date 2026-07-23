207699c79f50f2f0a18ac96cad84d3da4c2f2fb4
johannbm <johannes.moskvil@gmail.com>
2026-07-23 09:31:57 +0200

    Fiks vite mode

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
   },
   server: {
     cors: {
-      origin: ["https://www.intern.dev.nav.no", "http://localhost:9300"],
+      origin: ["https://www.intern.dev.nav.no", "https://arbeidsgiver.ekstern.dev.nav.no", "http://localhost:9300"],
     },
     origin: "http://localhost:5173",
   },
